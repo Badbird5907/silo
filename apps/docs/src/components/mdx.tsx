9ba5c2086dd6b6dc8ee594a936f8340304a3dfb4
Evan Yu <50347938+Badbird5907@users.noreply.github.com>
2026-04-22 18:04:45 -0400

    render icon in page tree

--- apps/docs/src/components/mdx.tsx
+++ apps/docs/src/components/mdx.tsx
@@ -8,6 +8,10 @@ import {
 } from "fumadocs-typescript/ui";
 import type { AutoTypeTableProps } from "fumadocs-typescript/ui";
 import type { MDXComponents } from "mdx/types";
+import { Nextjs } from "@/components/next-logo";
+import { TanStack } from "@/components/tanstack-logo";
+import { React } from "@/components/react-logo";
+import { Boxes } from "lucide-react";
 
 const generator = createGenerator({
   cache: createFileSystemGeneratorCache(".next/fumadocs-typescript"),
@@ -20,6 +24,10 @@ export function getMDXComponents(components?: MDXComponents) {
       <AutoTypeTable {...props} generator={generator} />
     ),
     ...components,
+    Nextjs,
+    TanStack,
+    React,
+    Boxes,
   } satisfies MDXComponents;
 }
 
--- apps/docs/src/lib/source.ts
+++ apps/docs/src/lib/source.ts
@@ -1,7 +1,19 @@
+import { Nextjs } from '@/components/next-logo';
+import { TanStack } from '@/components/tanstack-logo';
+import { React as ReactIcon } from "@/components/react-logo";
 import { docs } from 'collections/server';
 import { loader } from 'fumadocs-core/source';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
 
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) return;
+    if (icon === "Nextjs") return createElement(Nextjs)
+    if (icon === "TanStack") return createElement(TanStack)
+    if (icon === "React") return createElement(ReactIcon)
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons])
+  }
 });
